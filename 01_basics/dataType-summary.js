ae38a38a3efcdb3c121f08493fecdc8f4422264e
mustafakamran01 <kamran000mustafa@gmail.com>
2025-09-02 22:44:53 +0000

    memory

--- 01_basics/dataType-summary.js
+++ 01_basics/dataType-summary.js
@@ -19,23 +19,62 @@
     Reference (Non Primitive) data types:- Array, Objects, Functions
 */
 
-const colours = ["black", "blue", "green", "red"]  // it's an array of colours
+// const colours = ["black", "blue", "green", "red"]  // it's an array of colours
 
-let myObj = {
-    name: "Kamran",
-    age: 24,
-}
+// let myObj = {
+//     name: "Kamran",
+//     age: 24,
+// }
 
 // console.log(myObj);
 
 
-const myFunction = function(){
-    console.log("Hello World");
+// const myFunction = function(){
+//     console.log("Hello World");
     
+// }
+
+// console.log(typeof colours);        // object
+// console.log(typeof myObj);          // object
+// console.log(typeof myFunction);     // function
+
+
+//**********************************************Memory********************************************//
+
+/*
+    Primitive data type uses Stack memory and Non primitive data type uses heap memory.
+    In stack memory, an instance of a variable is used or given to another variable.
+    But, in heap memory the direct reference to the object is given
+*/
+
+
+// let myYoutubeName = "MustafaKamran"
+
+// let anotherName = myYoutubeName
+
+// anotherName = "Coding"
+
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+
+let user1 = {
+    email: "kamran@google.com",
+    name: "Kamran",
 }
 
-console.log(typeof colours);        // object
-console.log(typeof myObj);          // object
-console.log(typeof myFunction);     // function
+let user2 = user1
+
+user2.email = "kamran@microsoft.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
+
+
+
+
+
+
 
 
