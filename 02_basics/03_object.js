b3147b4d5ba2c225438cfdb89ae0b0d1e4ab2f99
mustafakamran01 <kamran000mustafa@gmail.com>
2025-09-04 12:48:24 +0000

    objects in JS

---
+++ 02_basics/03_object.js
@@ -0,0 +1,57 @@
+//**************Object literals*************//
+
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name: "Kamran",
+    "full name": "Mustafa Kamran",
+    [mySym]: "myKey1",
+    age: 24,
+    location: "Patna",
+    email: "kamran@apple.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Thursday"]
+}
+
+/*
+    Here all the keys are treated as string (name, full name, age, location, email, isLoggedIn, lastLoginDays)
+*/
+
+// console.log(jsUser.name);
+// console.log(jsUser["location"]);
+// console.log(jsUser["full name"]);
+// console.log(jsUser["isLoggedIn"]);
+
+/*
+    .(dot) and [] (sqaure brackets), both are ways to access the objects, but the recommended one is square bracket []
+*/
+
+// console.log(jsUser[mySym]);
+
+
+// jsUser.email = "kamran@samsung.com"     // this is how we can change the value of a key in object
+
+// Object.freeze(jsUser)                  // with the help of freez, now we cannot change the value of the object
+
+// jsUser.age = 30
+
+// console.log(jsUser);
+
+//***************Adding function in the object************//
+
+jsUser.greeting = function(){
+    console.log("Hello JS user");
+    
+}
+
+jsUser.anotherGreeting = function(){
+    console.log(`Hello JS user ${this.name}`);
+    
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.anotherGreeting());
+
+
+
+
