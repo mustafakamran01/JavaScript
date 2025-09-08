d54a7a3c4e94b9766a588754651db7bc9fca2a84
Mustafa Kamran <kamran000mustafa@gmail.com>
2025-09-08 09:54:02 +0000

    control flow

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,66 @@
+/* if and else statement */
+
+// const temperature = 47
+
+// if(temperature < 45){
+//     console.log("Quite hot weather");
+    
+// } else{
+//     console.log("Extreme hot weather");
+    
+// }
+
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "flying"
+//     console.log(`Power of a user is ${power}`);
+    
+// }
+// console.log(`Power of a user is ${power}`);
+// /*
+//     here it will give error, because the "power" is defined inside if scope
+// */
+
+
+//********************* Short hand notation *********************//
+
+// const amount = 1000
+
+// if(amount > 500) console.log("You have minimum balance in your account");
+
+
+
+const balance = 1100
+
+if(balance < 500){
+    console.log("Less than 500");
+
+} else if(balance < 750){
+    console.log("Less than 750");
+    
+} else if(balance < 900){
+    console.log("Less than 900");
+    
+} else{
+    console.log("Less than 1200");
+    
+}
+
+
+// const userLoggedIn = true
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = true
+
+// if(userLoggedIn && debitCard){
+//     console.log("Allow to buy a course");
+    
+// }
+
+// if(loggedInFromGoogle || loggedInFromEmail){
+//     console.log("User logged in");
+    
+// }
+
---
+++ 04_controlFlow/truthy.js
@@ -0,0 +1,62 @@
+// const userEmail = []
+
+// if(userEmail){
+//     console.log("Got user email");
+    
+// } else{
+//     console.log("Don't have user email");
+    
+// }
+
+// /*
+//     falsy value:
+//         false, 0, -0, BigInt -> 0n, "" (empty string), null, undefined, NaN
+// */
+
+// /*
+//     truthy value:
+//         "0" (it is zero as string), "false" (it is false as string), " " (string with space), 
+//         [] (empty array), {} (empty object), function(){} (empty function)
+// */
+
+// if(userEmail.length === 0){
+//     console.log("Array is empty");
+    
+// }
+
+
+// const myObj = {}
+
+// if (Object.keys(myObj).length === 0) {
+//     console.log("Object is empty");
+    
+// } else {
+    
+// }
+
+
+//**********************************Nullish Coalescing Operator (??) : null undefined*******************************//
+
+// let val1;
+
+// val1 = 5 ?? 10
+// val1 = null ?? 40
+// val1 = undefined ?? 65
+// val1 = null ?? 10 ?? 20
+// val1 = null ?? undefined
+
+// /*
+//     nullish operator will compare the values that are given, if the given values are null or undefined, they check the other value,
+//     if the other value is also null or undefined, then it will give undefined
+// */
+
+// console.log(val1);
+
+
+
+/****************************************** Terniary Operator ****************************************/
+
+const coffeePrice = 150
+
+coffeePrice >= 100 ? console.log("Coffee is expensive") : console.log("Coffee is affordable");
+
