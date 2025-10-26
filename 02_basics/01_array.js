0bed4f8b757e554aa3e44e60290fca7356fac105
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-26 15:20:09 +0530

    Array revision

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,14 +1,16 @@
-//*************************************How to declare an array*********************************//
+//************************************* How to declare an array *********************************//
 
-// const arr1 = [34, 76, 88, 6, "Kamran", true]
+const arr1 = [34, 76, 88, 6, "Kamran", true]
 // console.log(arr1);
 
-// const arr2 = new Array("Kamran", "Mustafa", 21, 100)
+const arr2 = new Array("Kamran", "Mustafa", 21, 100)
 // console.log(arr2);
 
+// console.log(typeof arr1);       // object
+// console.log(typeof arr2);       // object
 
 
-//*******************************************Array methods***************************************//
+//******************************************* Array methods ***************************************//
 
 const arr = [0, 1, 2, 3, 4, 5]
 
@@ -27,33 +29,29 @@ const arr = [0, 1, 2, 3, 4, 5]
 // console.log(`This will tell whether the given element is present or not: ${arr.includes(5)}`);
 
 // console.log(`This will give the index value of the given element: ${arr.indexOf(9)}`);
-/* If the given vaue is present then it will give the index value but if the given value is not present then it will give -1*/ 
+/* 
+    If the given vaue is present then it will give the index value but, 
+    if the given value is not present then it will give -1
+*/ 
 
 
-//*******************Slice and Splice***************//
+//******************* Slice and Splice ***************//
 
-console.log("A", arr);
+console.log("Original array: ", arr);
 
-const myn1 = arr.slice(1,3)
-console.log(myn1);
+let myn1 = arr.slice(0,4);
+console.log("Array after slicing: ", myn1);
 
-console.log("B", arr);
+console.log("Original array: ", arr);
 
-const myn2 = arr.splice(1,3)
-console.log(myn2);
+let myn2 = arr.splice(0,4);
+console.log("Array after splicing", myn2);
 
-console.log("C", arr);
+console.log("Original array", arr);
 
 /*
     In slice, we are getting a sub array of the given range and it will not effect the real array, but
     in splice they remove the element of given range from the real array and then give it to the user
 */
-
-
-
-
-
-
-
 
 
