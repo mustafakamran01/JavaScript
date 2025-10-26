8606cb46da90a83d1a59d5569c5b500c6881dc4f
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-26 11:50:02 +0530

    Revision

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,12 +1,17 @@
 // console.log(2 > 0);
 // console.log(2 < 0);
-// console.log(2 <= 0);
+// console.log(2 <= 0);n
 // console.log(2 >= 0);
 // console.log(2 == 0)
 
 
-// console.log("2" > 1)
-// console.log("02" > 1)   // it is not recommended to compare these type of equation, because sometimes it doesnot give the predicted result
+// console.log("2" < 1)
+// console.log("02" > 1)   
+
+/* 
+    It is not recommended to compare these type of equation, 
+    because sometimes it doesnot give the predicted result
+*/
 
 
 // console.log(null > 0);      // false
@@ -14,7 +19,7 @@
 // console.log(null >= 0);     // true
 
 /* 
-    the reaon is that an equality check (==) and comparisons (>, <, >=, <=) work differently.
+    The reaon is that an equality check (==) and comparisons (>, <, >=, <=) work differently.
     comparisons convert null value to a number, treating it as 0.
     that's why (null >= 0) is true and (null > 0) is false
 */
