ac63f40ef3ba518ce2bb2a12d117bec8fe45cf88
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-27 01:37:31 +0530

    IIFE Revision

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,22 +1,25 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-(function dataBase(){               // this is know as named IIFE, because it has a name
-    console.log("DB connected");
-})();
-
+// (function dataBase(){
+//     console.log("Database Connected");
+// })();
 /*
     whenever we are creating more than one IIFE, we need to put semicolon(;) after every IIFE
 */
 
-( () => {                               // this is normal IIFE
-    console.log("DB Connected 2");
-    
-})();
 
-( (name) => {                                       // this is IIFE with parameters
-    console.log(`Hey ${name} DB is connected`);
+
+// ( () => {                               // this is normal IIFE
+//     console.log("DB Connected 2");
     
-})("Kamran")
+// })();        // Both are correct ways
+// (() => console.log("Database connected 2"))();
+
+
+// ( (name) => {                                       // this is IIFE with parameters
+//     console.log(`Hey ${name} DB is connected`);
+    
+// })("Kamran")
 
 
 /*
