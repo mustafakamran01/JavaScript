047865503d33463e3d88a755e13999ec2ddf12d6
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-27 15:15:09 +0530

    Reduce Revision

--- 05_iterations/reduce.js
+++ 05_iterations/reduce.js
@@ -5,46 +5,43 @@ const myNum = [1, 2, 3, 4, 5]
 //     return acc + curr_val
 
 // }, 0)
-
 // console.log(total);
 
-// const total = myNum.reduce( (acc, curr_val) => {
-//     console.log(`Accumulator: ${acc} and Current value: ${curr_val}`);
-//     return acc + curr_val
-    
-// }, 0)
 
+// const total = myNum.reduce( (acc, currVal) => {
+//     console.log(`acc: ${acc} and currVal: ${currVal}`);
+//     return acc + currVal;
+// }, 0)
 // console.log(total);
 
 
 
 const shoppingCart = [
     {
-        item: "Shoes",
-        price: 4000
+        itemName: "Shirt",
+        itemPrice: 1500
     },
 
     {
-        item: "Watch",
-        price: 9000
+        itemName: "Watch",
+        itemPrice: 9000
     },
 
     {
-        item: "Hoodie",
-        price: 2000
+        itemName: "Hoodie",
+        itemPrice: 2000
     },
 
     {
-        item: "Shirt",
-        price: 2500
+        itemName: "Shoes",
+        itemPrice: 4500
     },
 
     {
-        item: "Cargo",
-        price: 2000
+        itemName: "Mobile",
+        itemPrice: 130000
     }
-]
+];
 
-const myTotal = shoppingCart.reduce( (acc, item) => acc + item.price, 0)
-
-console.log(myTotal);
+const total = shoppingCart.reduce( (acc, currItem) => acc + currItem["itemPrice"], 0)
+console.log(total);
