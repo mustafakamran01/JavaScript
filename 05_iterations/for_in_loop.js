cf39d2f86afa778e266647ee50645940b682e80d
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-27 11:10:19 +0530

    For in loop Revision

--- 05_iterations/for_in_loop.js
+++ 05_iterations/for_in_loop.js
@@ -1,44 +1,44 @@
-/************************* for in loop *******************/
+/************************* for in loop *************************/
 
-// const myObject = {
-//     suzuki: "Fronx",
-//     hyundai: "Creta",
-//     mahindra: "XUV700",
-//     vw: "Virtus"
-// }
+const myObject = {
+    suzuki: "Fronx",
+    hyundai: "Verna",
+    mahindra: "XUV700",
+    vw: "Virtus"
+}
 
-// for(const key in myObject){
+
+// for (const key in myObject){
 //     console.log(key);
-    
 // }
 /*
-    it will print the keys present in the object
+    It will print the keys present in the object
 */
 
-// for(const key in myObject){
+
+// for (const key in myObject){
 //     console.log(myObject[key]);
-    
 // }
 /*
-    it will only print the value associated with the keys
+    It will only print the value associated with the keys
 */
 
-// for(const key in myObject){
-//     console.log(`${key} has good car, named as ${myObject[key]}`);
-    
+
+// for (const key in myObject){
+//     console.log(`${key} have ${myObject[key]} car`);
 // }
+
 
 
 // const myCar = ["Fronx", "Verna", "Safari", "Taigun"]
 
 // for(const key in myCar){
-//     console.log(myCar[key]);
-    
+//     console.log(key);       // Here, key represent the index value.
 // }
 /*
     for in loop generally print the keys whereas for of loop print the values of the array
 */
-
+ 
 
 const map = new Map()
 
@@ -48,8 +48,7 @@ map.set("West Bengal", "Kolkata")
 
 for(const key in map){
     console.log(key);
-    
 }
 /*
-    it will print nothing, because maps are not iterable using for in loops
+    It will print nothing, because maps are not iterable using for in loops
 */
