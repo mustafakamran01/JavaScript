792114d60f28a8a385abc731068e80e929a10188
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-27 11:53:55 +0530

    For each loop Revision

--- 05_iterations/for_each_loop.js
+++ 05_iterations/for_each_loop.js
@@ -1,60 +1,74 @@
 /***************************** for each loop **************************/
 
-// const myArray = ["C", "C++", "Java", "Python", "JavaScript"]
+const myArray = ["C", "C++", "Java", "Python", "JavaScript"]
 
 // myArray.forEach( function (item){
-//     console.log(item);
-    
+//     console.log(`Element: ${item}`);
 // })
 /*
-    here we are not giving function name, because it's a call back function, and
+    Here we are not giving function name, because it's a call back function, and
     every time the loops iterate, functions goes to the array and take each element as a parameter, and
     here we are printing the functions arguments
 */
 
-// myArray.forEach( (item) => {         // using arrow function
-//     console.log(item);
-    
+
+// myArray.forEach( (item) => {                    // using arrow function
+//     console.log(`Element Arrow: ${item}`);
 // })
 
 
-// function printing(item){                // this is also a way to use for each loop by passing outside function as an argument
-//     console.log(item);
-    
+// function printing(item){
+//     console.log(`Element: ${item}`);
 // }
-// myArray.forEach(printing)
+// myArray.forEach(printing);
+/*
+    This is also a way to use for each loop by passing outside function as an argument
+*/
 
 
 // myArray.forEach( (item, index, arr) => {
-//     console.log(item, index, arr);
-    
+//     console.log(`${index} index have ${item} item`);
+//     console.log(arr);  
 // })
 /*
     function also contains index and whole array as an parameter
 */
 
 
+
 const myCoding = [
     {
         languageName: "JavaScript",
-        languageFileName: "js"
+        languageFileName: ".js"
     },
 
     {
         languageName: "Java",
-        languageFileName: "java"
+        languageFileName: ".java"
     },
 
     {
         languageName: "Python",
-        languageFileName: "py"
+        languageFileName: ".py"
+    },
+
+    {
+        languageName: "C++",
+        languageFileName: ".cpp"
     }
 ]
 
-myCoding.forEach( (item) => {
-    console.log(item.languageName);
-    
+// myCoding.forEach( (item) => {
+//     console.log(item.languageName);
+// })
+myCoding.forEach( (obj) => {
+    console.log(`Language name: ${obj.languageName}`);
+    console.log(`Language file name: ${obj.languageFileName}`);    
 })
 /*
-    this is how we can access array of objects
+    This is how we can access array of objects
+*/
+
+/*
+    We cannot use for each loop on object directly.
 */
