1d63be96fe422780d2df948b241cfb173bdf1ddb
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-27 22:33:35 +0530

    Object comparison

---
+++ Practice/Comparison_Of_Object.js
@@ -0,0 +1,19 @@
+// Input:
+// { a: 1, b: { c: 2 } }
+// { a: 1, b: { c: 2 } }
+
+// Output: true
+
+const Input1 = {
+    a: 1,
+    b: {
+        c: 2
+    }
+}
+
+const input2 = {
+    a: 1,
+    b: {
+        c:2
+    }
+}
