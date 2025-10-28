d13e64bc39d15c5fe71e1069bd7428f67f643646
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-28 13:42:36 +0530

    Practice Question

---
+++ Practice/Merge_Arrays_Without_Duplicate.js
@@ -0,0 +1,15 @@
+/* 
+    We need to Merge two arrays and remove duplicate elements.
+    
+    Example:
+    Input: [1, 2, 3], [3, 4, 5]
+    Output: [1, 2, 3, 4, 5] 
+*/
+
+let myArr1 = [1, 2, 3]
+let myArr2 = [3, 4, 5]
+
+const newArr = myArr1.filter( (ele) => !myArr2.includes(ele))
+                     .concat(myArr2)
+
+console.log(newArr);
