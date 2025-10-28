d6e8c60d44afba9d64fc88a80d9badef7581ff52
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-28 17:01:57 +0530

    Practice Question

--- Practice/Count_Word_Occurence.js
+++ Practice/Count_Word_Occurence.js
@@ -6,3 +6,19 @@
     Output: { I: 2, love: 2, coding: 1, and: 1, JavaScript: 1 }
 */
 
+let myStr = "I love coding and I love coding in javaScript";
+
+let strArr = myStr.split(" ");
+
+let wordCount = {}
+
+for (let ele of strArr){
+    // console.log(wordCount[ele]);
+    if (wordCount[ele]){
+        wordCount[ele]++;
+    } else {
+        wordCount[ele] = 1;
+    }
+}
+
+console.log(wordCount);
