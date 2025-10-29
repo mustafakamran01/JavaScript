0d81f8adfce14cb3dff6d9b5dd86d055bfa738cb
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-29 12:35:32 +0530

    Project 1 solution

--- Project_1/solution.js
+++ Project_1/solution.js
@@ -1,16 +1,8 @@
 const buttons = document.querySelectorAll('.button')
 
-buttons.forEach( (button)  => {
-    // console.log(button);
+buttons.forEach( (button) => {
     button.addEventListener('click', (e) => {
-        // console.log(e);
-        // console.log(e.target);
-        /*
-            e.target give the attribute of the target
-            here button is the target, so it gives the id, class etc attributes of that particular button
-        */
         document.body.style.backgroundColor = e.target.id
-        
     })
 })
 
