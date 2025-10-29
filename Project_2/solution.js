5811d4e88340ec268dea06f451859814b56e85de
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-29 13:49:58 +0530

    Arranging codes

--- Project_2/solution.js
+++ Project_2/solution.js
@@ -3,19 +3,24 @@ const button = document.querySelector('#calc')
 button.addEventListener('click', (e) => {
     e.preventDefault();
     
+    // Taking height and weight input from user
     const height = Number(document.querySelector('#height').value)
     const weight = Number(document.querySelector('#weight').value)
 
+    // Accessing the result element to display result
     const results =  document.querySelector('#results')
 
+    // Checking if the height and weight input is correct or not
     if (height <= 0 || isNaN(height)) {
         results.innerHTML = "Please enter correct height"
     } else if (weight <= 0 || isNaN(weight)) {
         results.innerHTML = "Please enter correct weight"
     } else {
 
+        // calculating BMI
         const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
+        // Showing result as per the BMI
         if (bmi < 18.6) {
             results.innerHTML = `${bmi} | You are under weight`
         } else if (bmi >= 18.6 && bmi <= 24.9) {
