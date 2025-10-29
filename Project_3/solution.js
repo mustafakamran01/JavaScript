a8a35e98315653184ba318f6dad558a48c3321f8
mustafakamran01 <kamran000mustafa@gmail.com>
2025-10-29 14:01:16 +0530

    Project 3 solution

--- Project_3/solution.js
+++ Project_3/solution.js
@@ -1,6 +1,5 @@
-const clock = document.getElementById('clock')
+const clock = document.querySelector('#clock')
 
-setInterval(function() {
-    let date = new Date
-    clock.innerHTML = date.toLocaleTimeString()
+setInterval( () => {
+    clock.innerHTML = new Date().toLocaleTimeString()
 }, 1000)
