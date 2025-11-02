d7d43eeaa081b4c81e7d93abad5b63ddbdc27b7a
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-03 01:45:58 +0530

    Static Props Revision

--- 09_oops/06_Inheritance.js
+++ 09_oops/06_Inheritance.js
@@ -6,6 +6,10 @@ class User {
     logMe() {
         return `Username is ${this.userName}`
     }
+
+    static createId(){
+        return `123`
+    }
 }
 
 class Teacher extends User {
@@ -24,4 +28,11 @@ const user1 = new Teacher("Kamran", "kamran@google.com", "1234321")
 console.log(user1);
 console.log(user1.addCourse());
 console.log(user1.logMe());
+// console.log(user1.createId());
+/* 
+    We cannot access functions which are static using instance of a class, 
+    but to access those methods, we need to access them with class name
+*/
+console.log(User.createId());
+
 
