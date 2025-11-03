10dd97b8484730f11741d05b1ffda1b963c2cdbb
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-04 00:32:38 +0530

    Set_Get Revision

--- 09_oops/09_Setter_Getter.js
+++ 09_oops/09_Setter_Getter.js
@@ -1,24 +1,40 @@
-class User{
-    constructor(password, email){
-        this.email = email
-        this.password = password
+/* 
+    Here we are creating class, and
+    setting and getting value with the help of set and get
+*/
+class User {
+    constructor(userName, email, password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
     }
 
-    set email(value){
-        this._email = value
+    set userName(userName) {
+        this._userName = userName;
     }
-    get email(){
-        return this._email.toUpperCase()
-    }
-
-    set password(value){
-        this._password = value
-    }
-    get password(){
-        return this._password.toUpperCase()
+    get userName() {
+        return `${this._userName}`.toUpperCase();
     }
 
+    set email (email) {
+        this._email = email;
+    }
+    get email() {
+        return `${this._email}`.toUpperCase();
+    }
+
+    set password(password) {
+        this._password = password;
+    }
+    get password() {
+        return `qwerty${this._password}afqty`
+    }
 }
-const user1 = new User('abc', 'mustafa@google.com')
-console.log(user1.email);     
-console.log(user1.password);     
+
+const newObj = new User("mustafa kamran", "kamran@google.com", "qwerty")
+console.log(newObj);
+console.log(newObj.email);
+console.log(newObj.password);
+
+
+
