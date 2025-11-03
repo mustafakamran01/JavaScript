a2b90adeb695fe161f11dcf9722dd073889837a5
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-03 23:46:41 +0530

    ObjectDescriptor Revision

--- 09_oops/08_MathPI.js
+++ 09_oops/08_MathPI.js
@@ -1,22 +1,27 @@
 // console.log(Object.getOwnPropertyDescriptor(Math, 'PI'))
-//  with this method, we get to know why we cannot change the value of math.pi
+/* 
+    Here we get the property which are not accessible generally like writable, enumerable, configurable.
+    Here the first parameter is Math(className) and second property is PI(property) 
+    So, here we get the property of PI (writable, enumerable, configurable)
+*/
 
 const myObj = {
-    name: "Chai",
-    price: '15',
-    isAvailable: true
+    name: "Mustafa Kamran",
+    email: "kamran@google.com",
+    company: "Google"
 }
+console.log(myObj);
 
-// console.log(Object.getOwnPropertyDescriptor(myObj, 'price'));
+Object.defineProperty(myObj, "name", {
+    writable: false
+})
+/* 
+    Here we change the property of our object, and we set writable to false,
+    so, now we cannot change the value of name property in myObj
+*/
 
-// Object.defineProperty(myObj, 'price', {
-//     writable: false,
-//     enumerable: false
-// })
+myObj.name = "Change"
 
-// console.log(Object.getOwnPropertyDescriptor(myObj, 'price'));
+console.log(myObj);
 
-for (const [key, value] of Object.entries(myObj)) {
-    console.log(`${key} : ${value}`);
-    
-}
+
