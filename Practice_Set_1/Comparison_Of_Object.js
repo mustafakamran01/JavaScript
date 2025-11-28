93cf3e50a8915faf0232e98c34a46c543a907caf
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-28 18:50:26 +0530

    Practice Question

--- Practice_Set_1/Comparison_Of_Object.js
+++ Practice_Set_1/Comparison_Of_Object.js
@@ -4,7 +4,7 @@
 
 // Output: true
 
-const Input1 = {
+const input1 = {
     a: 1,
     b: {
         c: 2
@@ -14,6 +14,22 @@ const Input1 = {
 const input2 = {
     a: 1,
     b: {
-        c:2
+        c: 2
     }
 }
+
+// console.log(input1);
+// console.log(input2)
+
+let input1Arr = Object.entries(input1)
+let input2Arr = Object.entries(input2)
+
+// console.log(input1Arr);
+// console.log(input2Arr);
+
+
+let keys1 = Object.keys(input1)
+let keys2 = Object.keys(input2)
+
+console.log(keys1);
+console.log(keys2);
