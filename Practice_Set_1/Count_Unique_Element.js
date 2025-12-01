8c13aebda588e23c32ba82f0a1fe294d8dfd6926
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-01 19:04:52 +0530

    Practice Question

--- Practice_Set_1/Count_Unique_Element.js
+++ Practice_Set_1/Count_Unique_Element.js
@@ -1,13 +1,29 @@
 /* We need to find out the number of unique elements present in an array */
 
-let myArr = [1, 2, 2, 3, 4, 4, 5]
+// let myArr = [1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6, 6]
 
-const mySet = new Set();
+// const mySet = new Set();
 
-for (let ele of myArr){
-    mySet.add(ele)
-}
+// for (let ele of myArr){
+//     mySet.add(ele)
+// }
 
-console.log(mySet.size);
+// // console.log(mySet.size);
+
+// let count = 0;
+
+// for (let i = 0; i < myArr.length - 1; i++) {
+//     // for (let j = 0; j < myArr.length; j++) {
+//         if (myArr[i] == myArr[i+1]) {
+//             count++;
+//         // }
+//     }
+// }
+
+// console.log(mySet.size - count);
+
+
+
+
 
 
