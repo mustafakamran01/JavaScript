2eb16cc1783bc0bc35c32abd5b83ee2fd3b7773b
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-07 22:43:03 +0530

    Practice Question

--- Practice_Set_1/Comparison_Of_Object.js
+++ Practice_Set_1/Comparison_Of_Object.js
@@ -20,33 +20,12 @@ const input2 = {
     s: 5
 }
 
-// // We are creating a function to compare both object recursively
-// function comparison(obj1, obj2) {
-
-//     if (obj1 === obj2) return true
-
-//     if (typeof(obj1) !== 'object' || typeof(obj2) !== 'object' || obj1 == null || obj2 == null) return false
-
-//     let keys1 = Object.keys(obj1)
-//     let keys2 = Object.keys(obj2)
-
-//     if (keys1.length != keys2.length) return false
-
-//     for (let key of keys1) {
-//         if (!comparison(obj1[key], obj2[key])) {
-//             return false
-//         }
-//     }
-
-//     return true
-// }
-
-// console.log(comparison(input1, input2));
-
+// We are creating a function to compare both object recursively
 function comparison(obj1, obj2) {
+
     if (obj1 === obj2) return true
 
-    if (obj1 == null || typeof(obj1) != 'object' || obj2 == null || typeof(obj2) != 'object') return false
+    if (typeof(obj1) !== 'object' || typeof(obj2) !== 'object' || obj1 == null || obj2 == null) return false
 
     let keys1 = Object.keys(obj1)
     let keys2 = Object.keys(obj2)
@@ -54,10 +33,13 @@ function comparison(obj1, obj2) {
     if (keys1.length != keys2.length) return false
 
     for (let key of keys1) {
-        if (!comparison(obj1[key], obj2[key])) return false
+        if (!comparison(obj1[key], obj2[key])) {
+            return false
+        }
     }
 
     return true
 }
 
 console.log(comparison(input1, input2));
+
