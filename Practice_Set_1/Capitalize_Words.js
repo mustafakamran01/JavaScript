77859502c351b98b49f89c7218ae06acc8b27411
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-07 13:53:05 +0530

    Practice Question

--- Practice_Set_1/Capitalize_Words.js
+++ Practice_Set_1/Capitalize_Words.js
@@ -5,7 +5,7 @@ let myStr = "my name is mustafa kamran"
 /* 
     Here, firstly we are spliting the string on the basis of space(" "), so that we can get each words in the form of array, then
     we are applying loop (map) on the array and taking first letter of each word and capitalizing it, and then
-    further concat rest letters in that.
+    further concat rest letters in that with the help of slice.
     After that we will each join words with space and again makie it a string
 */
 const capitalizedArr = myStr.split(" ")
