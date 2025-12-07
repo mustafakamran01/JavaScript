56d3d526c376d50d5f2e553c455fc957dd05658c
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-07 18:27:26 +0530

    Practice Question

--- Practice_Set_1/Combine_Two_Objects.js
+++ Practice_Set_1/Combine_Two_Objects.js
@@ -23,6 +23,7 @@ let myObj2 = {
     a: 10
 }
 
+// Here we are converting both objects into an array
 let myObj1Arr = Object.entries(myObj1)
 let myObj2Arr = Object.entries(myObj2)
 
@@ -30,6 +31,12 @@ let myObj2Arr = Object.entries(myObj2)
 // const newObj = Object.fromEntries(newArr)
 // console.log(newObj);
 
+/* 
+    Now we are applying map method on first object to iterate over it
+    Every iteration gives each keys and we will check whether that key present in second object or not.
+    If it is present then we will not insert that key-value into new array,
+    and then we will concat the second array into the new array
+*/
 const newObj = Object.fromEntries(myObj1Arr.filter( ([key]) => !myObj2.hasOwnProperty(key)).concat(myObj2Arr))
 console.log(newObj);
 
