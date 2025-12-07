2f85164d1b9f4bd1b4ba3026de63e9bf014505b6
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-07 22:42:33 +0530

    Practice Question

--- Practice_Set_1/Comparison_Of_Object.js
+++ Practice_Set_1/Comparison_Of_Object.js
@@ -42,3 +42,22 @@ const input2 = {
 // }
 
 // console.log(comparison(input1, input2));
+
+function comparison(obj1, obj2) {
+    if (obj1 === obj2) return true
+
+    if (obj1 == null || typeof(obj1) != 'object' || obj2 == null || typeof(obj2) != 'object') return false
+
+    let keys1 = Object.keys(obj1)
+    let keys2 = Object.keys(obj2)
+
+    if (keys1.length != keys2.length) return false
+
+    for (let key of keys1) {
+        if (!comparison(obj1[key], obj2[key])) return false
+    }
+
+    return true
+}
+
+console.log(comparison(input1, input2));
